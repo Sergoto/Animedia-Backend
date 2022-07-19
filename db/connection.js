06caf9d20ada8345a148766f72ce9534af94940e
Sergoto <Dklin22@gmail.com>
2022-07-19 19:13:00 -0400

    testing backend with dummy crud and getting connection/ENV going

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const mongoURI =
+  process.env.NODE_ENV === "production"
+    ? process.env.DB_URL
+    : process.env.DEV_DB_URL;
+
+mongoose
+  .connect(mongoURI)
+  .then((instance) =>
+    console.log(`Successfully connected to ${instance.connections[0].name}`)
+  )
+  .catch((err) => console.log(err));
+
+module.exports = mongoose;
+
+
